e50322ea044b06144fa7478c4d640ff77622711d
JulienT01 <julien.tj@gmail.com>
2024-02-19 17:15:41 +0100

    moving chain and gridworld to rlberry_scool

--- rlberry_scool/envs/finite/gridworld.py
+++ rlberry_scool/envs/finite/gridworld.py
@@ -1,15 +1,21 @@
-import rlberry
+import matplotlib
 import numpy as np
 
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
 from rlberry.envs.finite_mdp import FiniteMDP
 from rlberry_scool.envs.finite import gridworld_utils
+from rlberry.rendering import Scene, GeometricPrimitive, RenderInterface2D
+from rlberry.rendering.common_shapes import circle_shape
 
+
+import rlberry
 
 logger = rlberry.logger
 
 
-class GridWorldSimple(FiniteMDP):
+class GridWorld(RenderInterface2D, FiniteMDP):
     """
     Simple GridWorld environment.
 
@@ -103,8 +109,14 @@ class GridWorldSimple(FiniteMDP):
         FiniteMDP.__init__(
             self, self.R, self.P, initial_state_distribution=init_state_idx
         )
+        RenderInterface2D.__init__(self)
         self.reset()
         self.reward_range = (self.R.min(), self.R.max())
+
+        # rendering info
+        self.set_clipping_area((0, self.ncols, 0, self.nrows))
+        self.set_refresh_interval(100)  # in milliseconds
+        self.renderer_type = "pygame"
 
     @classmethod
     def from_layout(
@@ -327,8 +339,15 @@ class GridWorldSimple(FiniteMDP):
                     % (self.index2coord[next_s_idx] + (prob,))
                 )
 
+    def render_ascii(self):
+        print(self._build_ascii())
+
     def step(self, action):
         assert self.action_space.contains(action), "Invalid action!"
+
+        # save state for rendering
+        if self.is_render_enabled():
+            self.append_state_for_rendering(self.state)
 
         # take step
         next_state, reward, terminated, truncated, info = self.sample(
@@ -337,5 +356,135 @@ class GridWorldSimple(FiniteMDP):
         self.state = next_state
         return next_state, reward, terminated, truncated, info
 
-    def render_ascii(self):
-        print(self._build_ascii())
+    #
+    # Code for rendering
+    #
+    def get_layout_array(self, state_data=None, fill_walls_with=np.nan):
+        """
+        Returns an array 'layout' of shape (nrows, ncols) such that:
+
+            layout[row, col] = state_data[self.coord2idx[row, col]]
+
+        If (row, col) is a wall:
+
+            layout[row, col] = fill_walls_with
+
+        Parameters
+        ----------
+        state_data : np.array, default = None
+            Array of shape (self.observation_space.n,)
+        fill_walls_with : float, default: np.nan
+            Value to set in the layout in the coordinates corresponding to walls.
+
+        Returns
+        -------
+        Gridworld layout array of shape (nrows, ncols).
+        """
+        layout = np.zeros((self.nrows, self.ncols))
+        if state_data is not None:
+            assert state_data.shape == (self.observation_space.n,)
+            data_rows = [self.index2coord[idx][0] for idx in self.index2coord]
+            data_cols = [self.index2coord[idx][1] for idx in self.index2coord]
+            layout[data_rows, data_cols] = state_data
+        else:
+            state_rr, state_cc = self.index2coord[self.state]
+            layout[state_rr, state_cc] = 1.0
+
+        walls_rows = [ww[0] for ww in self.walls]
+        walls_cols = [ww[1] for ww in self.walls]
+        layout[walls_rows, walls_cols] = fill_walls_with
+        return layout
+
+    def get_layout_img(
+        self, state_data=None, colormap_name="cool", wall_color=(0.0, 0.0, 0.0)
+    ):
+        """
+        Returns an image array representing the value of `state_data` on
+        the gridworld layout.
+
+        Parameters
+        ----------
+        state_data : np.array, default = None
+            Array of shape (self.observation_space.n,)
+        colormap_name : str, default = 'cool'
+            Colormap name.
+            See https://matplotlib.org/tutorials/colors/colormaps.html
+        wall_color : tuple
+            RGB color for walls.
+        Returns
+        -------
+        Gridworld image array of shape (nrows, ncols, 3).
+        """
+        # map data to [0.0, 1.0]
+        if state_data is not None:
+            state_data = state_data - state_data.min()
+            if state_data.max() > 0.0:
+                state_data = state_data / state_data.max()
+
+        colormap_fn = plt.get_cmap(colormap_name)
+        layout = self.get_layout_array(state_data, fill_walls_with=np.nan)
+        norm = matplotlib.colors.Normalize(vmin=0.0, vmax=1.0)
+        scalar_map = cm.ScalarMappable(norm=norm, cmap=colormap_fn)
+        img = np.zeros(layout.shape + (3,))
+        for rr in range(layout.shape[0]):
+            for cc in range(layout.shape[1]):
+                if np.isnan(layout[rr, cc]):
+                    img[self.nrows - 1 - rr, cc, :] = wall_color
+                else:
+                    img[self.nrows - 1 - rr, cc, :3] = scalar_map.to_rgba(
+                        layout[rr, cc]
+                    )[:3]
+        return img
+
+    def get_background(self):
+        """
+        Return a scene (list of shapes) representing the background
+        """
+        bg = Scene()
+
+        # walls
+        for wall in self.walls:
+            y, x = wall
+            shape = GeometricPrimitive("POLYGON")
+            shape.set_color((0.25, 0.25, 0.25))
+            shape.add_vertex((x, y))
+            shape.add_vertex((x + 1, y))
+            shape.add_vertex((x + 1, y + 1))
+            shape.add_vertex((x, y + 1))
+            bg.add_shape(shape)
+
+        # rewards
+        for y, x in self.reward_at:
+            flag = GeometricPrimitive("POLYGON")
+            rwd = self.reward_at[(y, x)]
+            color = 0.5 * np.abs(rwd) / self.reward_range[1]
+            if rwd > 0:
+                flag.set_color((0.0, color, 0.0))
+            if rwd < 0:
+                flag.set_color((color, 0.0, 0.0))
+
+            x += 0.5
+            y += 0.25
+            flag.add_vertex((x, y))
+            flag.add_vertex((x + 0.25, y + 0.5))
+            flag.add_vertex((x - 0.25, y + 0.5))
+            bg.add_shape(flag)
+
+        return bg
+
+    def get_scene(self, state):
+        """
+        Return scene (list of shapes) representing a given state
+        """
+        y, x = self.index2coord[state]
+        x = x + 0.5  # centering
+        y = y + 0.5  # centering
+
+        scene = Scene()
+
+        agent = circle_shape((x, y), 0.25, n_points=5)
+        agent.type = "POLYGON"
+        agent.set_color((0.75, 0.0, 0.5))
+
+        scene.add_shape(agent)
+        return scene
